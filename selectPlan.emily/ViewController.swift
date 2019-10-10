5121f19d3a05979a42d48b53ca17edaf936b33c9
appleEmily <emiri3128@gmail.com>
2019-10-11 01:27:19 +0900

    Initial Commit

---
+++ selectPlan.emily/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  selectPlan.emily
+//
+//  Created by 野崎絵未里 on 2019/10/11.
+//  Copyright © 2019 emily.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var textFirst: UITextField!
+    @IBOutlet weak var textSecond: UITextField!
+    @IBOutlet weak var textThird: UITextField!
+    @IBOutlet weak var textForth: UITextField!
+    @IBOutlet weak var textFifth: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
